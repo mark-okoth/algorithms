a70a7c089aaede1cbe70dde0ee87462a992c84e1
mark odhiambo okoth <markokoth96@gmail.com>
2020-01-23 21:20:38 +0300

    linked list

---
+++ linkedList.py
@@ -0,0 +1,36 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.nextNode = None
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def insertStart(self, data):
+        self.size = self.size + 1
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+        else:
+            newNode.nextNode = self.head
+            self.head = newNode  
+    def size1(self):
+        return self.size
+
+
+
+LinkedList = LinkedList()
+
+LinkedList.insertStart(11)
+(LinkedList.insertStart(45))
+
+
+
+
+
+
+
+    
