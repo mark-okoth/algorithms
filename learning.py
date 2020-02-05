b736cf7760d8ca328c0f6e5ba5f4152a5b39c7b6
mark odhiambo okoth <markokoth96@gmail.com>
2020-02-05 17:22:35 +0300

    refreshing on matplotlib knowledge

---
+++ learning.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+x = [1,2,3,4,5]
+y = [22,33,44,55,66]
+
+plt.plot(x,y)
+plt.legend('the')
+plt.title('the heading of the plot')
+
+plt.xlabel('x line')
+plt.ylabel('y line')
+
+plt.show()
